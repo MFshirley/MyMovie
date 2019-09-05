f1dd7570eb8a6df2eb766552e84db8c89ceae0d0
shirley <shirley@methink.cn>
2019-09-05 15:19:07 +0800

    add clound database matchreview

---
+++ cloudfunctions/matchReview/index.js
@@ -0,0 +1,22 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: 'moviecloud-0yjsg',
+  traceUser: true,
+})
+
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  const user = wxContext.OPENID
+
+  const reviewRes = await db.collection('review').doc(id).where({
+    user,
+  }).get()
+  const review = reviewRes.data
+
+  return review
+}
